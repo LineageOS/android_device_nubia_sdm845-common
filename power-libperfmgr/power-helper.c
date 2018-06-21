41c01764dcaad0064728f74640b985ea7ac38d09
Michael Wright <michaelwr@google.com>
2018-06-21 02:38:28 +0100

    sdm845-common: Update power HAL to version 1.3
    This adds support for the EXPENSIVE_RENDERING power hint.
    
    BUG: 110112323
    Test: adb shell cat /sys/class/kgsl/kgsl-3d0/devfreq/cur_freq
          Verified GPU frequency reset by adb shell pkill surfaceflinger and system_server
    Change-Id: Ia1f6e99b67c5e88b610412ad30f152d241095a48
    Merged-In: Ia1f6e99b67c5e88b610412ad30f152d241095a48

--- power-libperfmgr/power-helper.c
+++ power-libperfmgr/power-helper.c
@@ -28,7 +28,7 @@
  */
 
 #define LOG_NIDEBUG 0
-#define LOG_TAG "android.hardware.power@1.2-service.nubia_sdm845-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.nubia_sdm845-libperfmgr"
 
 #include <errno.h>
 #include <inttypes.h>
