448e53a3a01405a5ebe7b07a1cff0a45b108bcef
Ethan Chen <intervigil@gmail.com>
2019-02-03 15:12:39 -0800

    sdm845-common: Disable verity and descriptor checking
    * avbtool takes a --flags argument, not --flag
    * avbtool provides an option to disable hashtree checking
    
    Change-Id: Ib57d8ebed69a0998005563a9268f9fc736cbeaf7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -158,6 +158,7 @@ BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
 
 # WiFi
