083ed1497a10029edfec376786a0bc8d501ad68b
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2020-12-03 13:09:27 +0530

    sdm845-common: Include hardware/google/interfaces
     * Required by power-libperfmgr
    
    Change-Id: I80c820a54f53976c9c1f91815f402719f7ee9e65
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>

--- sdm845.mk
+++ sdm845.mk
@@ -316,6 +316,7 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
     hardware/google/pixel \
     vendor/qcom/opensource/usb/etc
 
