f2cb586017eb0e2496ddb88ef4d709773916e4ea
Cyborg2017 <wangyang0987654@gmail.com>
2023-05-13 18:59:56 +0800

    sdm845-common: Build android.hardware.gnss@2.0 from source
    Change-Id: I57bc147fc6eb4313ef8f30655b0170d9fe6591cc

--- sdm845.mk
+++ sdm845.mk
@@ -168,6 +168,10 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.nubia_sdm845
 
 # GPS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@2.0-impl-qti \
+    android.hardware.gnss@2.0-service-qti
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
     $(LOCAL_PATH)/gps/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
