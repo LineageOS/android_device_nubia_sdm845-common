LineageOS/android_device_xiaomi_sdm845-common

sdm845-common: Ditch prebuilt kernel support completely
It's not like we're ever gonna need this again.

Change-Id: I1d1392db2ad7c7c666ba4255889cea20bb5b1a8b

BoardConfigCommon.mk
@@ -39,9 +39,7 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_ARCH := arm64
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-  TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm845
-endif
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm845
 
 # Platform
 TARGET_BOARD_PLATFORM := sdm845

LineageOS/android_device_nubia_sdm845-common

BoardConfigCommon.mk
@@ -41,9 +41,7 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_ARCH := arm64
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-  TARGET_KERNEL_SOURCE := kernel/nubia/sdm845
-endif
+TARGET_KERNEL_SOURCE := kernel/nubia/sdm845
 
 # Platform
 TARGET_BOARD_PLATFORM := sdm845