b7cc24cb9e169c4427dcb7451b172e1f22a4f0e7
LuK1337 <priv.luk@gmail.com>
2023-10-20 00:53:15 +0200

    sdm845-common: Build libprotobuf-cpp-lite-3.9.1-vendorcompat
    Change-Id: I0aac742db9c2b4b3e48ee20158f19405863893c2

--- sdm845.mk
+++ sdm845.mk
@@ -167,7 +167,8 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
-    android.hardware.drm-service.clearkey
+    android.hardware.drm-service.clearkey \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 # Fingerprint
 PRODUCT_PACKAGES += \
