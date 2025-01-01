1f43c8cac8235e502e8546a379ce5d2657a40f7c
Michael Bestas <mkbestas@lineageos.org>
2025-01-01 04:42:57 +0200

    sdm845-common: Update display flags & packages
    Required after display HAL blueprint conversion
    
    Change-Id: Ib8ae3114ffae6ea5457cb9829fbbecfd7f065b66

--- sdm845.mk
+++ sdm845.mk
@@ -150,7 +150,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \
     android.hardware.graphics.mapper@2.0-impl-qti-display \
     gralloc.sdm845 \
-    hwcomposer.sdm845 \
+    hwcomposer.qcom \
     libdisplayconfig.qti \
     libqdMetaData \
     libtinyxml \
