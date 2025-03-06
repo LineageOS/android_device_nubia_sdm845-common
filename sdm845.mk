a8e2d43ee3c2d5c9930949aa42ca84389e36b5a0
me-cafebabe <me.cafebabe@gmail.com>
2025-03-07 03:10:38 +0500

    sdm845-common: Migrate to QTI USB Gadget AIDL HAL
    Change-Id: I6adbca2fab9a3fe0477d17f9786166828b786710

--- sdm845.mk
+++ sdm845.mk
@@ -277,7 +277,9 @@ PRODUCT_USE_VNDK_OVERRIDE := true
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.dual_role_usb
+    android.hardware.usb@1.3-service.dual_role_usb \
+    android.hardware.usb.gadget-service.qti \
+    usb_compositions.conf
 
 # VNDK
 PRODUCT_COPY_FILES += \
